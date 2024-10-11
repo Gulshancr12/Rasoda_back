7d7f4cdd53589ebb230b6d4e9325425722ec2be7
gulshancr12 <gulshancr12@gmail.com>
2024-10-11 23:18:19 +0530

    Add .gitignore and remove ignored files

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,7 +5,8 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Use environment variable for frontend URL
-const frontend_url = process.env.FRONTEND_URL || "http://localhost:5173";
+const frontend_url = process.env.FRONTEND_URL || "https://rasoda-front.vercel.app";
+
 
 // Placing User content from frontend
 const placeOrder = async (req, res) => {
